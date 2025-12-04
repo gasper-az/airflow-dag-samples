2a92d8b2a1791822d601058631732fb92048316e
Gaspar Acevedo <acevedo.zain.gaspar@gmail.com>
2025-12-04 16:58:53 -0300

    [DAGs] Update tutorial dag - using 2.8.4-python3.9

--- dags/tutorial_dag.py
+++ dags/tutorial_dag.py
@@ -1,20 +1,36 @@
 # SOURCE: https://airflow.apache.org/docs/apache-airflow/stable/tutorial/fundamentals.html#airflow-101-building-your-first-workflow
+# SOURCE: https://airflow.apache.org/docs/apache-airflow/2.8.4/tutorial/fundamentals.html
 import textwrap
 from datetime import datetime, timedelta
 
-# Operators; we need this to operate!
-from airflow.providers.standard.operators.bash import BashOperator
-
 # The DAG object; we'll need this to instantiate a DAG
-from airflow.sdk import DAG
+from airflow.models.dag import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
 with DAG(
     "tutorial",
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
     default_args={
         "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
         "retries": 1,
         "retry_delay": timedelta(minutes=5),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'end_date': datetime(2016, 1, 1),
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'trigger_rule': 'all_success'
     },
     description="A simple tutorial DAG",
     schedule=timedelta(days=1),
@@ -41,7 +57,7 @@ with DAG(
     You can document your task using the attributes `doc_md` (markdown),
     `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
     rendered in the UI's Task Instance Details page.
-    ![img](https://imgs.xkcd.com/comics/fixing_problems.png)
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
     **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
     """
     )
